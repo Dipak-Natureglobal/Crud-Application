d9f9de612ffe0539f7bed1c7eddb253acb837881
suraj <suraj@DESKTOP-SBV46DB>
2024-01-08 07:59:06 +0530

    Add project files.

---
+++ Todolist/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todolist.Models;
+namespace Todolist.Data;
+public class ApplicationDbContext : DbContext
+{
+     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+}
---
+++ Todolist/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Todolist.Models
+{
+    public class Category
+    {
+        // control+ . using system.compoenent etc
+        [Key]
+        public int Id { get; set; }
+        [Required]        
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+
+    }
+}
